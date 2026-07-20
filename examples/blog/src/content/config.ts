5a05740759300c6703d82ccf7b6fd800b9558c5a
PINOER <K162000080@gmail.com>
2026-07-20 22:13:16 +0800

    Update config.ts

--- examples/blog/src/content/config.ts
+++ examples/blog/src/content/config.ts
@@ -2,14 +2,17 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
 	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		lang: z.enum(['en', 'es', 'de', 'ja', 'zh']).default('en'), // 支持的多语言列表
-	}),
+	// 1. 将 schema 改为接收 ({ image }) 的函数
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			// 2. 将 z.string().optional() 改为 image().optional()
+			heroImage: image().optional(), 
+			lang: z.enum(['en', 'es', 'de', 'ja', 'zh']).default('en'), // 支持的多语言列表
+		}),
 });
 
 export const collections = { blog };
