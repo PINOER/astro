d84f31c224375c3b70c93830662518a1d8650d31
PINOER <K162000080@gmail.com>
2026-07-23 18:36:00 +0800

    Update consts.ts

--- examples/blog/src/consts.ts
+++ examples/blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'HelpMini — Micro AI Tools & Efficiency Utility Box';
+export const SITE_TITLE = 'HelpMini';
 export const SITE_DESCRIPTION = 'Discover lightweight micro AI tools and prompts to boost your productivity instantly at HelpMini.com.';
